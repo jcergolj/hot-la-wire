55836c9d0f35a94841d8bbc81a4463f667e202d8
Janez Cergolj <me@jcergolj.me.uk>
2021-01-13 10:51:34 +0100

    hotwire/turbo installation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script type="module">
+      import Turbo from 'https://cdn.skypack.dev/pin/@hotwired/turbo@v7.0.0-beta.2-ou6dW2bg0qdKgUED7QEB/min/@hotwired/turbo.js';
+    </script>
 </head>
 </head>
 
